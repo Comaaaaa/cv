1585ce02f443ff2d0678d0c94dbd986cd53475ab
Souf <sofiane.bassaler@epitech.eu>
2022-07-05 15:50:55 +0200

    [FIX] Grammar

--- src/components/Abilities/Experience.js
+++ src/components/Abilities/Experience.js
@@ -7,11 +7,10 @@ const Experience = () => {
             <div className="exp-1">
                 <h4>Graphiste - Freelancer</h4>
                 <h5>Septembre 2020 - Actuel </h5>
-                <p>Je m'occupe de réaliser les visuels artistiques des
-                    entreprises qui me solicitent, en effet je réalise des logos,
-                    bannières et sites vitrines afin de proposer des visuels qui
-                    correspondent à leurs images et en même temps attirer de
-                    nouveaux clients vers elles.</p>
+                <p>Je réalise des visuels artistiques pour des
+                    entreprises, comme des logos,
+                    des bannières ainsi que des sites vitrine. Je leur propose des visuels qui
+                    correspondent à leurs images afin qu'elles attirent de nouveaux clients.</p>
             </div>
             <div className="exp-2">
                 <h4>Commercial - Go Sport</h4>
@@ -20,12 +19,12 @@ const Experience = () => {
                 et leurs proposais des produits adaptés à leurs besoins.</p>
             </div>
             <div className="exp-3">
-                <h4>Hôte d'accueil et conseiller de clientèle - Société Générale</h4>
+                <h4>Hôte d'accueil et conseiller clientèle - Société Générale</h4>
                 <h5>Décembre 2019 - Avril 2020 </h5>
-                <p>Hôte d'accueil et conseiller de clientèle, je renseignais les clients
-                vis à vis de leurs besoin, j'analysais les portefeuilles clients et proposais
-                des solutions et services adaptés à leurs besoins, j'analysais les clients prospects
-                et proposais des offres commerciales afin d'attirer les clients auprès de l'établissement.</p>
+                <p>Hôte d'accueil et conseiller clientèle, je renseignais les clients
+                en fonction de leurs besoins, j'évaluais les portefeuilles clients et proposais
+                des solutions et services adaptés, j'analysais les dossiers des clients prospects
+                et les démarchais via des offres commerciales.</p>
             </div>
         </div>
     );
