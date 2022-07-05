7bffa3aafba440ff25821c020354162c600358af
Souf <sofiane.bassaler@epitech.eu>
2022-07-05 16:01:13 +0200

    [FIX] Editing the code for the required file

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,7 +5,7 @@ export const portfolioData = [
     languages: ['javascript','react'],
     languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
     source: 'https://github.com/Comaaaaa/cv',
-    info: "En effet ! le site sur lequel vous vous trouvez actuellement à été fait par moi, vous pourrez trouvez le code en cliquant sur le bouton présent ci-dessus, il s'agit de mon premier projet en react",
+    info: "En effet ! le site sur lequel vous vous trouvez actuellement a été réalisé par mes soins. Vous pourrez trouver le code en cliquant sur le bouton ci-dessus. Il s'agit de mon premier projet en react",
     picture: './media/site.png'
   },
   {
@@ -14,7 +14,7 @@ export const portfolioData = [
     languages: ['javascript'],
     languagesIcons: ['fab fa-js','fab fa-bitcoin'],
     source: 'https://github.com/Comaaaaa/cryptobot',
-    info: "Création d'un bot sur la plateforme discord, ce dernier est capable d'interagir avec l'utilisateur et ses commandes en proposant différents choix, ce dernier a été réalisé grâce à l'API de coinGecko, ainsi le bot est capable de donner le prix des cryptomonnaies en temps réel et dans la devise du choix de l'utilisateur, il est également capable de fournir des articles sur les cryptomonnaies.",
+    info: "Création d'un bot sur la plateforme Discord. Il est capable d'interagir avec l'utilisateur et ses commandes en proposant différents choix. Il a été réalisé grâce à l'API de coinGecko. Ainsi le bot est capable de donner le prix des cryptomonnaies en temps réel et dans la devise du choix de l'utilisateur. Il est également capable de fournir des articles de presse sur les cryptomonnaies.",
     picture: './media/bot.png'
   },
   {
@@ -23,7 +23,7 @@ export const portfolioData = [
     languages: ['C'],
     languagesIcons: ['fab fa-cuttlefish', 'fas fa-paint-brush'],
     source: 'https://github.com/Comaaaaa/rpg',
-    info: "Création d'un jeu en langage C grâce à la bibliothèque SFML, il s'agissait d'un projet de groupe, ainsi sur ce projet, je me suis occupé de la réalisation des sprites de jeu ainsi que des particules, j'ai ainsi appris le travail de groupe et la réalisation de sprites afin de faciliter le travail de mes coéquipiers.",
+    info: "Création d'un jeu en langage C grâce à la bibliothèque SFML. Il s'agissait d'un projet de groupe. Ainsi sur ce projet, je me suis occupé de la réalisation des sprites de jeu ainsi que des particules. Par ce projet, j'ai donc appris le travail de groupe et la réalisation de sprites afin de faciliter le travail de mes collaborateurs.",
     picture: './media/rpg.jpg'
   },
   {
@@ -31,7 +31,7 @@ export const portfolioData = [
     name: 'Epytodo',
     languages: ['javascript', 'SQL'],
     languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fas fa-database'],
-    info: "Création d'une app type TodoList en javaScript, cette dernière utilise une base de données en SQL sur ses utilisateurs, mais également sur les tâches effectuées, elle possède également une interface de connexion, toute la partie back-end et front-end a été faite par nous-mêmes. Malheureusement, le code source n'est pas disponible.",
+    info: "Création d'une app type TodoList en javaScript. Elle utilise une base de données en SQL sur ses utilisateurs ainsi que sur les tâches effectuées. Elle possède également une interface de connexion. Toute la partie back-end et front-end a été réalisée par mes soins. Malheureusement, le code source n'est pas disponible.",
     picture: './media/EpytodoCV.png'
   },
 ]
